2679deffa5f4bb0b944723443b20e897cda470c4
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-01 12:40:25 +0200

    API consumption : get patient by age group

--- src/components/PatientStatistics/PatientStatistics.js
+++ src/components/PatientStatistics/PatientStatistics.js
@@ -1,41 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import {
+  PATIENT_STATISTICS_LABELS,
+  PATIENT_STATISTICS_BACKGROUNDCOLOR
+} from '../../constants/constants';
+import healthy from '../../api/healthy';
 
-const data = {
-  labels: [
-    '',
-    '[10,15]',
-    '[16,20]',
-    '[21,25]',
-    '[26,30]',
-    '[31,35]',
-    '[36,40]',
-    '[41,45]',
-    '[46,50]',
-    '[51,55]',
-    '[56,60]',
-    ''
-  ],
-  datasets: [
-    {
-      data: [0, 10, 10, 5, 10, 8, 16, 10, 9, 4, 6],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(70, 215, 132, 0.6)',
-        'rgba(100, 140, 64, 0.6)',
-        'rgba(50, 10, 200, 0.6)'
-      ]
-    }
-  ]
-};
 const useStyles = makeStyles(theme => ({
   typography: {
     margin: 'auto'
@@ -46,6 +18,39 @@ const useStyles = makeStyles(theme => ({
 }));
 export default function PatientStatistics() {
   const classes = useStyles(); //add styles to variable classes
+  const [group, setGroup] = useState([]);
+  useEffect(() => {
+    const getPatientByAgeRange = async () => {
+      const authStr = `Bearer ${localStorage.getItem('token')}`;
+      const response = await healthy.get(`statistics/age`, {
+        headers: { Authorization: authStr }
+      });
+
+      setGroup(response.data.countGender);
+    };
+    getPatientByAgeRange();
+  });
+  const data = {
+    labels: PATIENT_STATISTICS_LABELS,
+    datasets: [
+      {
+        data: [
+          0,
+          group['[10-15]'],
+          group['[16-20]'],
+          group['[21-25]'],
+          group['[26-30]'],
+          group['[31-35]'],
+          group['[36-40]'],
+          group['[41-45]'],
+          group['[46-50]'],
+          group['[51-55]'],
+          group['[56-60]']
+        ],
+        backgroundColor: PATIENT_STATISTICS_BACKGROUNDCOLOR
+      }
+    ]
+  };
   return (
     <React.Fragment>
       <Typography component="h2" variant="h6" color="primary" gutterBottom>
