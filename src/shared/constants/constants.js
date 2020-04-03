c1ae5430eb97f8976304916fbf76a7a1fd684196
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-03 17:42:30 +0200

    contants,strings files

---
+++ src/shared/constants/constants.js
@@ -0,0 +1,194 @@
+/**
+ * Constant used in the PatientStatistics component
+ * the labels of the chart (bar)
+ */
+export const PATIENT_STATISTICS_LABELS = [
+  '',
+  '[10,15]',
+  '[16,20]',
+  '[21,25]',
+  '[26,30]',
+  '[31,35]',
+  '[36,40]',
+  '[41,45]',
+  '[46,50]',
+  '[51,55]',
+  '[56,60]',
+  ''
+];
+/**
+ * Constant used in the PatientStatistics component
+ * The background color of the chart (bar)
+ */
+export const PATIENT_STATISTICS_BACKGROUNDCOLOR = [
+  'rgba(255, 99, 132, 0.6)',
+  'rgba(54, 162, 235, 0.6)',
+  'rgba(255, 206, 86, 0.6)',
+  'rgba(75, 192, 192, 0.6)',
+  'rgba(153, 102, 255, 0.6)',
+  'rgba(255, 159, 64, 0.6)',
+  'rgba(255, 99, 132, 0.6)',
+  'rgba(70, 215, 132, 0.6)',
+  'rgba(100, 140, 64, 0.6)',
+  'rgba(50, 10, 200, 0.6)'
+];
+/**
+ * Constant used in the Dashboard component
+ * The background color of the chart (Ingredient)
+ */
+export const DASHBOARD_INGREDIENT_BACKGROUNDCOLOR = 'rgba(54, 162, 235, 0.6)';
+/**
+ * Constant used in the Dashboard component
+ * The name of the chart (Ingredient)
+ */
+export const DASHBOARD_INGREDIENT_NAME = 'Ingredients';
+/**
+ * Constant used in the Dashboard component
+ * The background color of the chart (Menu)
+ */
+export const DASHBOARD_MENU_BACKGROUNDCOLOR = 'rgba(255, 99, 132, 0.6)';
+/**
+ * Constant used in the Dashboard component
+ * The name of the chart (Menu)
+ */
+export const DASHBOARD_MENU_NAME = 'Menus';
+/**
+ * Constant used in the Dashboard component
+ * The title of the MenuBar component
+ */
+export const DASHBOARD_MENU_BAR_TITLE = 'Dashboard';
+/**
+ * Constant used in the Chart component
+ * the label 'Homme' of the chart component
+ */
+export const CHART_LABEL_MALE = 'Homme';
+/**
+ * Constant used in the Chart component
+ * the label 'Femme' of the chart component
+ */
+export const CHART_LABEL_FEMALE = 'Femme';
+/**
+ * Constant used in the Chart component
+ * The background color of the chart ('Homme','Femme')
+ */
+export const CHART_BACKGROUNDCOLOR = [
+  'rgba(255, 99, 132, 0.6)',
+  'rgba(54, 162, 235, 0.6)'
+];
+/**
+ * The title  of the Patient component
+ */
+export const PATIENT_MENU_BAR_TITLE = 'Patient';
+/**
+ * placeholder used in the patient component
+ */
+export const PATIENT_PLACEHOLDER = 'Chercher un patient';
+/**
+ * The title of the AddPatient component
+ */
+export const ADD_PATIENT_TITLE = 'Creer un patient';
+
+/**
+ * Constant message : use when field is required
+ */
+export const MESSAGE_VALIDATORS_REQUIRED = 'Ce champ est requis';
+/**
+ * Constant message : min length password
+ */
+export const MESSAGE_VALIDATORS_PASSWORD =
+  'Le mot de passe doit contenir au moins 8 caractères.';
+/**
+ * Constant message : email validation
+ */
+export const MESSAGE_VALIDATORS_EMAIL = "L'email n'est pas valide";
+/**
+ * The title of the Ingredient page
+ */
+export const INGREDIENTS_MENU_BAR_TITLE = 'Ingredients';
+/**
+ * The title of the AddIngredient component
+ */
+export const ADD_INGREDIENT_TITLE = 'Ajouter un ingredient';
+/**
+ * Constant message : is integer validation
+ */
+export const MESSAGE_VALIDATORS_INTEGER = 'Ce champ doit être un nombre';
+/**
+ * The title of the Update page
+ */
+export const MENU_BAR_UPDATE_TITLE = 'Modifier';
+/**
+ * Constant used in the AddRecommendation Page
+ * name of the creation
+ */
+export const ADDRECOMMENDATION_STEPPER_CREATION = 'Crée une Recommendation';
+/**
+ * Constant used in the AddRecommendation,UpdateRecommendation Page
+ *
+ */
+export const RECOMMENDATION_STEPPER_ADD = 'Ajouter des menus';
+/**
+ * Constant used in the UpadateRecommendation Page
+ * name of the creation
+ */
+export const UPDATERECOMMENDATION_STEPPER_CREATION = 'Modifier Recommandation';
+/**
+ * The title of the menus page
+ */
+export const MENUS_TITLE = 'Menus';
+/**
+ * The title of the AddMenu component
+ */
+export const ADD_MENU_TITLE = 'Ajouter un menu';
+/**
+ * Constant used in the AddMenu Page
+ * name of creation
+ */
+export const ADDMENU_STEPPER_CREATION = 'Crée un menu';
+/**
+ * Constant used in the AddMenu Page
+ *
+ */
+export const ADDMENU_STEPPER_ADD = 'Ajouter des ingredients au menu';
+/**
+ * The title of the Profil component
+ */
+export const PROFIL_TITLE = 'Profil';
+
+/**
+ * The message of the dialog component related to recommendation
+ */
+export const DIALOG_RECOMMENDATION = 'cette recommandation';
+
+/**
+ * The message of the dialog component related to ingredients
+ */
+export const DIALOG_INGREDIENT = 'ce ingredient';
+
+/**
+ * The message of the dialog component related to patient
+ */
+export const DIALOG_PATIENT = 'ce patient';
+
+/**
+ * The message of the dialog component related to patient
+ */
+export const DIALOG_MENU = 'ce menu';
+/**
+ * Primary color
+ */
+export const PRIMARY_COLOR = 'primary';
+/**
+ * Secondary color
+ */
+export const SECONDARY_COLOR = 'secondary';
+/**
+ * Constant used in the AddMenu Page
+ * name of the creation
+ */
+export const MENU_STEPPER_CREATION = 'Crée un menu';
+/**
+ * Constant used in the AddMenu Page
+ *
+ */
+export const MENU_STEPPER_ADD = 'Ajouter des ingredients au menu';
