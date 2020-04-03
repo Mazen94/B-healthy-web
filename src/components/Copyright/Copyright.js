c5829a6e5b714b3ac336009f2a42d94ae35591a1
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-03 17:44:28 +0200

    add the contants and the strings for Copyright component

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { B_HEALTHY } from '../../shared/strings/strings';
 /**
  * Component for showing Copyright at the end of each page
  */
@@ -8,9 +9,7 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        B-healthy
-      </Link>
+      <Link color="inherit">{B_HEALTHY}</Link>
       {new Date().getFullYear()}
     </Typography>
   );
