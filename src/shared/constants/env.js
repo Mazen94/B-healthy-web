b380b3ba0c51030c587c9f08c879aed223926463
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-06 15:15:56 +0200

    add a new headers parameter for the function axiosService

---
+++ src/shared/constants/env.js
@@ -0,0 +1,1 @@
+export const headers = `Bearer ${localStorage.getItem('token')}`; //Prepare the authorization with the token
--- src/shared/services/services.js
+++ src/shared/services/services.js
@@ -1,14 +1,14 @@
 import { URL_API } from '../constants/constants';
 import Axios from 'axios';
 
-export async function axiosService(endPoint, method, data) {
-  const authStr = `Bearer ${localStorage.getItem('token')}`; //Prepare the authorization with the token
+export async function axiosService(endPoint, method, headers, data) {
   const config = {
     method: method,
     url: `${URL_API}${endPoint}`,
     data: data,
-    headers: { Authorization: authStr },
   };
+  if (data) config.data = data;
+  if (headers) config.headers = { Authorization: headers };
   try {
     const response = await Axios(config);
     return response;
