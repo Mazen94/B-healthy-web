70577d8766171f2d86cd00243d278a521823158d
elayebMazen <el.ayeb.mazen@gmail.com>
2020-04-16 19:20:14 +0200

    New strings

--- src/shared/strings/strings.js
+++ src/shared/strings/strings.js
@@ -86,6 +86,7 @@ export const YEARS = 'Ans';
 export const ADD_INGREDIENT = 'Ingredient ajouté';
 export const UPDATE_INDREDIENT = 'Ingredient modifié ';
 export const ADD_RECOMMENDATION = 'Recommendation Ajoutée';
+export const UNAUTHENTICATED = 'Unauthenticated.';
 export const TABLE_HEAD_MENUS = [MENUS, MENU_TYPE, CALORIES, AGE_RANGE];
 export const TABLE_HEAD_INGREDIENTS = [INGREDIENTS, CALORIES, AMOUNT];
 export const TABLE_HEAD_PATIENTS = [
