053bdcb33f0225cb4cd25c451c45a55513342c91
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-02 16:41:09 +0200

    change the textsecondary per TEXT_SECONDARY constant

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { B_HEALTHY } from '../../shared/strings/strings';
+import { TEXT_SECONDARY } from '../../shared/constants/constants';
 /**
  * Component for showing Copyright at the end of each page
  */
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" color={TEXT_SECONDARY} align="center">
       {'Copyright © '}
       <Link color="inherit">{B_HEALTHY}</Link>
       {new Date().getFullYear()}
