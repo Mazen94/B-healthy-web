e4653990284ae5464e4af9d88828abc6cc4013d5
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-08 16:52:52 +0200

    add new props

--- src/components/AlertComponent/AlertComponent.js
+++ src/components/AlertComponent/AlertComponent.js
@@ -2,14 +2,19 @@ import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
 import React from 'react';
 
-const AlertComponent = ({ openSnackbar, handleCloseSnackbar, message }) => {
+const AlertComponent = ({
+  openSnackbar,
+  handleCloseSnackbar,
+  message,
+  severity = 'success',
+}) => {
   return (
     <Snackbar
       open={openSnackbar}
       autoHideDuration={2000}
       onClose={handleCloseSnackbar}
     >
-      <Alert onClose={handleCloseSnackbar} severity="success">
+      <Alert onClose={handleCloseSnackbar} severity={severity}>
         {message}
       </Alert>
     </Snackbar>
