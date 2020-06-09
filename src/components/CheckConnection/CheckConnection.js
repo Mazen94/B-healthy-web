2cc851210df121fed13fbcaa48e449c1d1c6658c
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-09 14:38:05 +0200

    add path_dashboard constant

--- src/components/CheckConnection/CheckConnection.js
+++ src/components/CheckConnection/CheckConnection.js
@@ -1,5 +1,6 @@
 import { Route, Redirect } from 'react-router-dom';
 import React from 'react';
+import { PATH_DASHBOARD } from '../../routes/path';
 /**
  * Component to check if the user connects or not
  * if the token exists in the localstorage then the user cannot access the page
@@ -9,9 +10,9 @@ export const CheckConnection = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={props =>
+      render={(props) =>
         localStorage.getItem('token') ? (
-          <Redirect to="/dashboard" />
+          <Redirect to={PATH_DASHBOARD} />
         ) : (
           <Component {...props} />
         )
