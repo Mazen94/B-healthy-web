28e1e34b66164ac6f733fd535d30d8e26db022f7
elayebMazen <el.ayeb.mazen@gmail.com>
2020-06-09 13:17:03 +0200

    add the rules name

--- src/shared/constants/validation.js
+++ src/shared/constants/validation.js
@@ -17,3 +17,9 @@ export const MESSAGE_VALIDATORS_INTEGER = 'Ce champ doit être un nombre';
 
 // Constant message : age validation
 export const MESSAGE_VALIDATORS_AGE = "L'age doit etre entre 5 et 90 ans";
+
+// NAME OF RULES
+export const RULES_NAME_IS_INTEGER = 'isInteger';
+export const RULES_NAME_LENGHT_PASSWORD = 'lenghPassword';
+export const RULES_NAME_VALIDATION_AGE = 'validationAge';
+export const RULES_NAME_REQUIRED = 'required';
